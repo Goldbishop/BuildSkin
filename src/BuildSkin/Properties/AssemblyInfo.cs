739aa73f687ffdabf8a53a7441a8f8ec61257ef2
John <3235535+Goldbishop@users.noreply.github.com>
2022-01-06 07:57:30 -0600

    Moved Project Srouce under 'src' directory

---
+++ src/BuildSkin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuildSkin")]
+[assembly: AssemblyDescription("A tool to make LOTRO skins by combining individual elements and adapting them to your resolution.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mevordel")]
+[assembly: AssemblyProduct("BuildSkin")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("BuildSkin")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cda2ece5-15d2-4943-b16a-a2f70d10f373")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.99.9.20")]
+[assembly: AssemblyFileVersion("0.99.9.20")]
