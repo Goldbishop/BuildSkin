1f998286b3317fdfa58b58118da740b1c111f037
John <3235535+Goldbishop@users.noreply.github.com>
2022-01-06 08:59:04 -0600

    Added Modify Copyright

--- src/BuildSkin/mainForm.cs
+++ src/BuildSkin/mainForm.cs
@@ -706,15 +706,19 @@ namespace buildSkin {
 
 		private void goToMevordel ( object o , LinkLabelLinkClickedEventArgs e ) {
 			//TODO: Make Real Website :)
+
 			//Process.Start("http://mevordel.usgrd.com/");
 		}
 
 		private void goToMITLicense ( object o , LinkLabelLinkClickedEventArgs e ) {
-			MessageBox.Show ( "Copyright (c) 2011-2012 Samuel Holland\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE." , "License" );
+			MessageBox.Show (
+				"Modify: Copyright (c) 2022" + ( ( DateTime.Now.Year != 2022 ) ? $"-{DateTime.Now.Year}" : "" ) + " John Wood (Goldbishop)\n\n" +
+				"Original: Copyright (c) 2011-2012 Samuel Holland\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE." , "License" );
 		}
 
 		private void openReadMe ( object o , LinkLabelLinkClickedEventArgs e ) {
 			//TODO: Write README
+
 			//Open Default Handler For Text Files
 			Process.Start ( new FileInfo ( "Readme.txt" ).FullName );
 		}
