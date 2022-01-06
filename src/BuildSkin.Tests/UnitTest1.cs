f2814f5a985b978ba53d8137ae83d2af5c5f9cd5
John <3235535+Goldbishop@users.noreply.github.com>
2022-01-06 08:22:27 -0600

    Added Unit Test Project

---
+++ src/BuildSkin.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildSkin.Tests {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void TestMethod1 ( ) {
+		}
+	}
+}
